c0d0634b17f1627e803b888505bddaa941cd350a
1750777402 <l>
2020-03-15 23:47:24 +0800

    update

--- ygz_aspen/server/src/main/java/com/ygz/aspen/model/User.java
+++ ygz_aspen/server/src/main/java/com/ygz/aspen/model/User.java
@@ -1,32 +1,12 @@
 package com.ygz.aspen.model;
 
+import lombok.Data;
+
+@Data
 public class User {
 
     private Long id;
 
     private String username;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                '}';
-    }
 }
--- ygz_aspen/web/src/main/java/com/ygz/aspen/WebApplication.java
+++ ygz_aspen/web/src/main/java/com/ygz/aspen/WebApplication.java
@@ -1,14 +1,17 @@
 package com.ygz.aspen;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class WebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
-        System.out.println("aspen is running");
+        ConfigurableApplicationContext context = SpringApplication.run(WebApplication.class, args);
+        log.info("------------------ ygz-aspen is running on {} ------------------",context.getEnvironment().getActiveProfiles()[0]);
     }
 
 }
