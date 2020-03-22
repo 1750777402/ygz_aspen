2c74580db0d39a4ada94139a0767266822eced1b
1750777402 <l>
2020-03-22 15:51:30 +0800

    基本配置修改

--- ygz_aspen/server/src/main/java/com/ygz/aspen/model/User.java
+++ ygz_aspen/server/src/main/java/com/ygz/aspen/model/User.java
@@ -5,8 +5,18 @@ import lombok.Data;
 @Data
 public class User {
 
-    private Long id;
+    private Long userId;
 
-    private String username;
+    private String uname;
+
+    private String unick;
+
+    private String password;
+
+    private Integer isDeleted;
+
+    private Long createTime;
+
+    private Long updateTime;
 
 }
