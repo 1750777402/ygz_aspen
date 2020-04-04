1ea91373a5b7d8896c7b342ff8ea325c8669e81d
1750777402 <l>
2020-04-04 15:34:08 +0800

    init

--- ygz_aspen/web/src/main/java/com/ygz/aspen/controller/HellowController.java
+++ ygz_aspen/web/src/main/java/com/ygz/aspen/controller/HellowController.java
@@ -1,6 +1,5 @@
 package com.ygz.aspen.controller;
 
-import com.ygz.aspen.model.User;
 import com.ygz.aspen.service.RedisService;
 import com.ygz.aspen.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +15,9 @@ public class HellowController {
     @Autowired
     private RedisService redisService;
 
-    @GetMapping("/getUserById")
-    public User getUserById(@RequestParam("userId") Long userId){
-        return userService.getUserById(userId);
-    }
-
-    @GetMapping("/getCache")
+    @GetMapping("/")
     public String getCache(@RequestParam("key") String key){
-        return redisService.get(key);
+        return redisService.get("ygz_aspen");
     }
 
 }
