09c0ee53f21e71644fdd0a62a57a09ae7a0b646e
1750777402 <l>
2020-04-04 13:33:59 +0800

    logback配置

--- ygz_aspen/web/src/main/java/com/ygz/aspen/WebApplication.java
+++ ygz_aspen/web/src/main/java/com/ygz/aspen/WebApplication.java
@@ -11,7 +11,7 @@ public class WebApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(WebApplication.class, args);
-        log.info("------------------ ygz-aspen is running on {} ------------------",
+        log.info("------------------ ygz_aspen is running on {} ------------------",
                 context.getEnvironment().getActiveProfiles()[0]);
     }
 
