77cdf6643b157478506efcc2684750da04406ff6
1750777402 <l>
2020-04-04 15:35:15 +0800

    init

--- ygz_aspen/web/src/main/java/com/ygz/aspen/controller/HellowController.java
+++ ygz_aspen/web/src/main/java/com/ygz/aspen/controller/HellowController.java
@@ -1,7 +1,6 @@
 package com.ygz.aspen.controller;
 
 import com.ygz.aspen.service.RedisService;
-import com.ygz.aspen.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,13 +9,10 @@ import org.springframework.web.bind.annotation.*;
 public class HellowController {
 
     @Autowired
-    private UserService userService;
-
-    @Autowired
     private RedisService redisService;
 
     @GetMapping("/")
-    public String getCache(@RequestParam("key") String key){
+    public String getCache(){
         return redisService.get("ygz_aspen");
     }
 
