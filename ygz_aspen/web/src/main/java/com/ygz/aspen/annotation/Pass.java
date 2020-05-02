d672c027802ebcd87d14db58c38743e662331a30
1750777402 <l>
2020-05-02 17:39:48 +0800

    jwt相关内容修改完善

--- ygz_aspen/web/src/main/java/com/ygz/aspen/annotation/Pass.java
+++ ygz_aspen/web/src/main/java/com/ygz/aspen/annotation/Pass.java
@@ -4,6 +4,10 @@ import java.lang.annotation.*;
 
 /**
  * 在Controller方法上加入该注解不会验证身份
+ * 注意一个点  在标注了此注解的地方可以获取当前登录用户信息
+ * 但能不能成功获取取决于请求有没有带认证token信息
+ * 如果带了就可以正常获取，如果不带则会抛出空指针
+ * 所以建议在标注了此注解的方法中获取当前用户信息时加上异常捕获，做好异常处理
  * @author : ygz
  */
 @Target( { ElementType.METHOD } )
